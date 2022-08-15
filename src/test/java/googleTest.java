0fcc435d89b68a3be74f1e01197b4d9b243e1f3e
hewoen <62847789+hewoen@users.noreply.github.com>
2022-08-15 17:15:06 +0200

    Update googleTest.java

--- src/test/java/googleTest.java
+++ src/test/java/googleTest.java
@@ -16,7 +16,7 @@ public class googleTest {
         String path = "/usr/bin/chromedriver";
         System.setProperty("webdriver.chrome.driver", path);
         ChromeOptions options = new ChromeOptions();
-        options.setHeadless(true);
+        //options.setHeadless(true);
         WebDriver driver = new ChromeDriver(options);
         driver.get("https://google.com");
         WebElement akzeptieren = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[3]/span/div/div/div/div[3]/div[1]/button[2]/div"));
