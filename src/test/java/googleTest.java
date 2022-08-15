2fe9968cc98b9fd6f027b81333ae10d1a669c2d6
hewoen <62847789+hewoen@users.noreply.github.com>
2022-08-15 17:38:06 +0200

    Update googleTest.java

--- src/test/java/googleTest.java
+++ src/test/java/googleTest.java
@@ -2,8 +2,8 @@ import com.google.common.annotations.VisibleForTesting;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
